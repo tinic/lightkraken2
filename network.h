28be2e7a8a627208ca9aa6c7427deb643876c78d
Tinic Uro <tinicuro@gmail.com>
2023-10-22 20:26:13 -0700

    Cleanup.

--- network.h
+++ network.h
@@ -57,10 +57,15 @@ class Network {
     
     uint32_t murmur3_32(const uint8_t* key, size_t len, uint32_t seed) const;
 
+#ifndef BOOTLOADER
     static constexpr char hostname_base[] = "lightkraken-";
+#else  // #ifndef BOOTLOADER
+    static constexpr char hostname_base[] = "lightkraken-bootloader-";
+#endif  // #ifndef BOOTLOADER
     static constexpr char hex_table[16] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',};
+    static constexpr size_t id_length = 8;
 
-    char hostname[sizeof(hostname_base)+8] = {};
+    char hostname[sizeof(hostname_base) + id_length + 1] = {};
     uint8_t macaddr[6] = {};
 };
 
