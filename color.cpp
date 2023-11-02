f1112fc16dfd41f47d5cfc11aaecee08bb3bb772
Tinic Uro <tinicuro@gmail.com>
2023-11-02 07:24:09 -0700

    Test

--- color.cpp
+++ color.cpp
@@ -61,7 +61,7 @@ void ColorSpaceConverter::setRGBColorSpace(const RGBColorSpace &rgbSpace) {
         3.2404542f, -1.5371385f, -0.4985314f, -0.9692660f, 1.8760108f, 0.0415560f, 0.0556434f, -0.2040259f, 1.0572252f,
     };
 
-    concatMatrix(ledl2srgbl, ledl2srgbl, xyz2srgbl);
+    concatMatrix(ledl2srgbl, ledl2srgbl, xyz2srgbl); 
 
     static float srgbl2xyz[9] = {
         0.4124564f, 0.3575761f, 0.1804375f, 0.2126729f, 0.7151522f, 0.0721750f, 0.0193339f, 0.1191920f, 0.9503041f,
